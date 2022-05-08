e44d1b1a697e7e2961684b7e20d63826757a0618
GermanBenavidesLeon <manchitobena@gmail.com>
2022-05-08 15:51:24 -0300

    cambio de imagenes, y generar javascript para el idioma

--- Js/idiomas.js
+++ Js/idiomas.js
@@ -1,8 +1,52 @@
-//--- Boton Idioma ---//
+let botonIngles = document.getElementById("btn_ingles")
+let botonEspañol = document.getElementById("btn_español")
+function checkLanguage(){
+    const ingles = document.getElementsByClassName("english")
+    const español = document.getElementsByClassName("spanish")
+    const footerIngles = document.getElementsByClassName("english_footer")
+    const footerEspañol = document.getElementsByClassName("spanish_footer")
+    if(localStorage.getItem('lng')==1){
+        for(let i=0;i<español.length;i++){
+            español[i].style.display = "block"
+            ingles[i].style.display = "none"
+        }
+        for(let i=0;i<footerEspañol.length;i++){
+            footerEspañol[i].style.display = "block"
+            footerIngles[i].style.display = "none"
+        }
+        
+    }else if (localStorage.getItem('lng')==2){
+        for(let i=0;i<español.length;i++){
+            español[i].style.display = "none"
+            ingles[i].style.display = "block"
+        }
+        for(let i=0;i<footerEspañol.length;i++){
+            footerEspañol[i].style.display = "none"
+            footerIngles[i].style.display = "block"
+        }
 
-// const btnIdiomas = document.querySelector('.idioma_btn')
+    }
+}
+botonEspañol.addEventListener('click', () =>{
+    localStorage.setItem("lng", 1)
+    checkLanguage()
 
-// btnIdiomas.addEventListener('click', () => {
-//     document.body.classList.toggle('english');
-//     btnIdiomas.classList.toggle('active')
-// });
+})
+botonIngles.addEventListener('click', () =>{
+    localStorage.setItem("lng", 2)
+    checkLanguage()
+
+})
+
+
+
+// const botonMenu = document.getElementsByClassName("boton")
+// const menu = document.getElementsByClassName('navbarList')
+
+// for (boton of botonMenu){
+//     boton.addEventListener('click', () => {
+//         for(let i=0;i<menu.length;i++){
+//             menu[i].classList.toggle('menu_visible')
+//         }
+//     })
+// }
