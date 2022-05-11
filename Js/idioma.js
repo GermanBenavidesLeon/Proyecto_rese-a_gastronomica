a0896cf263718dbe7e3a5ddf6bbd43707c440cad
GermanBenavidesLeon <manchitobena@gmail.com>
2022-05-10 22:36:02 -0300

    ajuste Js para slider e idioma

--- Js/idioma.js
+++ Js/idioma.js
@@ -1,8 +1,73 @@
-// --- FUNCIONES IDIOMAS ---- //
-
 let botonIngles = document.getElementById("btn_ingles")
 let botonEspañol = document.getElementById("btn_español")
-
+function setSliders(){
+    if(localStorage.getItem('lng')!=2){
+        checkLanguage()
+        let slider = document.querySelector(".slider_container")
+        let sliderPagina = document.querySelectorAll(".contenido_slider")
+        let contador = 1;
+        let width = sliderPagina[0].clientWidth;
+        let intervalo = 3000;
+        
+        window.addEventListener("resize", function(){
+            width = sliderPagina[0].clientWidth;
+        })
+        
+        
+    
+        function slides () {
+            slider.style.transform = "translate("+(-width*contador)+"px)";
+            slider.style.transition = "transform .8s";
+            contador ++;
+        
+            if(contador == sliderPagina.length){
+                setTimeout(function() {
+                    slider.style.transform = "translate(0px)";
+                    slider.style.transition = "transform 0s";
+                    contador=1;
+                }, 1500)
+            }
+        }
+        
+        setInterval(function(){
+            slides();
+        },intervalo);
+    }
+    else{
+        checkLanguage()
+        let slider = document.querySelector(".slider_container__eng")
+        let sliderPagina = document.querySelectorAll(".contenido_slider__eng")
+        let contador = 1;
+        let width = sliderPagina[0].clientWidth;
+        let intervalo = 3000;
+        
+        window.addEventListener("resize", function(){
+            width = sliderPagina[0].clientWidth;
+        })
+        
+        
+        
+    
+        function slides () {
+            slider.style.transform = "translate("+(-width*contador)+"px)";
+            slider.style.transition = "transform .8s";
+            contador ++;
+        
+            if(contador == sliderPagina.length){
+                setTimeout(function() {
+                    slider.style.transform = "translate(0px)";
+                    slider.style.transition = "transform 0s";
+                    contador=1;
+                }, 1500)
+            }
+        }
+        
+        setInterval(function(){
+            slides();
+        },intervalo);
+    
+    }
+}
 function checkLanguage(){
     const ingles = document.getElementsByClassName("english")
     const español = document.getElementsByClassName("spanish")
@@ -41,68 +106,3 @@ botonIngles.addEventListener('click', () =>{
     setSliders()
 
 })
-
-// ---- FUNCIONES PARA SLIDERS ---- ///
-function setSliders(){
-    if(localStorage.getItem('lng')!=2){
-        checkLanguage()
-        let slider = document.querySelector(".slider_container")
-        let sliderPagina = document.querySelectorAll(".contenido_slider")
-        let contador = 1;
-        let width = sliderPagina[0].clientWidth;
-        let intervalo = 3000;
-        
-        window.addEventListener("resize", function(){
-            width = sliderPagina[0].clientWidth;
-        })
-    
-        function slides () {
-            slider.style.transform = "translate("+(-width*contador)+"px)";
-            slider.style.transition = "transform .8s";
-            contador ++;
-        
-            if(contador == sliderPagina.length){
-                setTimeout(function() {
-                    slider.style.transform = "translate(0px)";
-                    slider.style.transition = "transform 0s";
-                    contador=1;
-                }, 1500)
-            }
-        }
-        
-        setInterval(function(){
-            slides();
-        },intervalo);
-    }
-    else{
-        checkLanguage()
-        let slider = document.querySelector(".slider_container__eng")
-        let sliderPagina = document.querySelectorAll(".contenido_slider__eng")
-        let contador = 1;
-        let width = sliderPagina[0].clientWidth;
-        let intervalo = 3000;
-        
-        window.addEventListener("resize", function(){
-            width = sliderPagina[0].clientWidth;
-        })
-    
-        function slides () {
-            slider.style.transform = "translate("+(-width*contador)+"px)";
-            slider.style.transition = "transform .8s";
-            contador ++;
-        
-            if(contador == sliderPagina.length){
-                setTimeout(function() {
-                    slider.style.transform = "translate(0px)";
-                    slider.style.transition = "transform 0s";
-                    contador=1;
-                }, 1500)
-            }
-        }
-        
-        setInterval(function(){
-            slides();
-        },intervalo);
-    
-    }
-}
